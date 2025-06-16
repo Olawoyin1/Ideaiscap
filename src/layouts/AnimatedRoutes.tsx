5457c56ecce98f1545d163d3c6ebb6a2811c1334
Olawoyin <yustee2017@gmail.com>
2025-06-16 10:31:57 +0100

    loader effect - new

--- src/layouts/AnimatedRoutes.tsx
+++ src/layouts/AnimatedRoutes.tsx
@@ -19,7 +19,15 @@ export default function AnimatedRoutes() {
     <Suspense
       fallback={
         <div className="fixed inset-0 flex bg-black items-center justify-center text-white text-xl">
-          Loading...
+          <div id="load">
+  <div>G</div>
+  <div>N</div>
+  <div>I</div>
+  <div>D</div>
+  <div>A</div>
+  <div>O</div>
+  <div>L</div>
+</div>
         </div>
       }
     >
